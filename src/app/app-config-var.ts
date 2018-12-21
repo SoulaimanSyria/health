3c6d0ccc5cc4d6535553644faec30efac560625d
SoulaimanSyria <solaiman.991@hotmail.com>
2018-12-21 21:42:18 +0200

    change server

--- src/app/app-config-var.ts
+++ src/app/app-config-var.ts
@@ -6,5 +6,6 @@ export const APP_CONFIG: InjectionToken<AppConfig>
   = new InjectionToken('app.config');
 
 export const AppConfiguration: AppConfig = {
-  apiBaseUrl: 'http://localhost:63482/',
+  //apiBaseUrl: 'http://localhost:63482/',
+  apiBaseUrl: 'http://www.awbhealthsystem.somee.com/',
 }
