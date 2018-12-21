cf4bd40f0a8ca70b81d984ae3794c497214399c6
SoulaimanSyria <solaiman.991@hotmail.com>
2018-12-22 01:13:12 +0200

    ch

--- src/app/app-config-var.ts
+++ src/app/app-config-var.ts
@@ -6,6 +6,5 @@ export const APP_CONFIG: InjectionToken<AppConfig>
   = new InjectionToken('app.config');
 
 export const AppConfiguration: AppConfig = {
-  //apiBaseUrl: 'http://localhost:63482/',
   apiBaseUrl: 'http://www.awbhealthsystem.somee.com/',
 }
